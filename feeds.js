49b963a99989b35ed76b720f7572ca9ad751bcc5
Tony Martinez <ajosephmartinez@gmail.com>
2020-04-12 13:46:05 -0700

    Nba feed

---
+++ feeds.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+
+module.exports.nba = async function (event, context) {
+  console.log("queryStringParameters", event.queryStringParameters.date);
+  const date = event.queryStringParameters.date;
+  const res = await axios.get(
+    `http://data.nba.net/data/10s/prod/v1/${date}/scoreboard.json`
+  );
+  console.log("res?", res.data);
+
+  return {
+    // return null to show no errors
+    statusCode: 200, // http status code
+    body: JSON.stringify({
+      games: res.data,
+    }),
+  };
+};

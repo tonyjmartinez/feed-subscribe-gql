48f7e2f7ce10e25dfa072d5b6333fedad2f0a24b
Tony Martinez <ajosephmartinez@gmail.com>
2020-04-12 14:09:19 -0700

    .

--- feeds.js
+++ feeds.js
@@ -1,4 +1,32 @@
 const axios = require("axios");
+const teams = require("./data/teams");
+
+const getNumberWithOrdinal = (n) => {
+  var s = ["th", "st", "nd", "rd"],
+    v = n % 100;
+  return n + (s[(v - 20) % 10] || s[v] || s[0]);
+};
+const findTeam = (teams, teamId) =>
+  _.find(teams, (team) => team.teamId === teamId);
+const getGameState = (game) => {
+  const { isGameActivated, period, clock, startTimeEastern, statusNum } = game;
+  const currentPeriod = getNumberWithOrdinal(period.current);
+  if (isGameActivated) {
+    if (period.isHalftime) return "Halftime";
+    if (period.isEndOfPeriod) return `End of the ${currentPeriod}`;
+    if (statusNum === 1) return `Game Start`;
+    if (period.current > 4) {
+      return period.current === 5 ? "OT" : `${period.current - 4} OT`;
+    }
+    return `${clock} ${currentPeriod}`;
+  }
+  if (statusNum === 1) {
+    return startTimeEastern;
+  }
+  if (statusNum === 3) {
+    return "Final";
+  }
+};
 
 module.exports.nba = async function (event, context) {
   console.log("queryStringParameters", event.queryStringParameters.date);
@@ -7,12 +35,36 @@ module.exports.nba = async function (event, context) {
     `http://data.nba.net/data/10s/prod/v1/${date}/scoreboard.json`
   );
   console.log("res?", res.data);
+  const games = _.get(res, "data.games.games");
+  const scores = games.map((game) => {
+    const { hTeam, vTeam, isGameActivated } = game;
+    const homeTeamInfo = findTeam(teams, hTeam.teamId);
+    const visitingTeamInfo = findTeam(teams, vTeam.teamId);
+    const HomeLogo = logos[homeTeamInfo.tricode];
+    const VisitingLogo = logos[visitingTeamInfo.tricode];
+    console.log("getgamestate", getGameState(game));
+    const active = isGameActivated;
+    return {
+      gameState: getGameState(game),
+      home: {
+        score: hTeam.score,
+        name: `${homeTeamInfo.tricode} ${homeTeamInfo.nickname}`,
+        logo: <HomeLogo size={35} />,
+      },
+      visitor: {
+        score: vTeam.score,
+        name: `${visitingTeamInfo.tricode} ${visitingTeamInfo.nickname}`,
+        logo: <VisitingLogo size={35} />,
+      },
+      active,
+    };
+  });
 
   return {
     // return null to show no errors
     statusCode: 200, // http status code
     body: JSON.stringify({
-      games: res.data,
+      games: scores,
     }),
   };
 };
